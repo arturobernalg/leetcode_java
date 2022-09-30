a8f75b58493ffe9c84eaac1380f94f51ff4c4dd8
Arturo Bernal <arturobernalg@gmail.com>
2022-09-30 21:59:21 +0200

    Make public

--- src/main/java/node/TreeNode.java
+++ src/main/java/node/TreeNode.java
@@ -1,20 +1,23 @@
 package node;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode() {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
     }
 
-    TreeNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
+
+
 }
