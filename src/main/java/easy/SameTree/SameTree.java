1558842305037816f3473e5c3c0d97251b2f74a8
Arturo Bernal <arturobernalg@gmail.com>
2022-10-05 21:37:18 +0200

    Solve same tree

---
+++ src/main/java/easy/SameTree/SameTree.java
@@ -0,0 +1,22 @@
+package easy.SameTree;
+
+import node.TreeNode;
+
+public class SameTree {
+
+
+    public static  boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val
+                && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right));
+
+    }
+}
